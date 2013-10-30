250010dd78d9d1e9c96217f105d95f22e2ee52c8
anthonysutardja <anthony.sutardja@gmail.com>
2013-10-30 11:14:43 -0700

    completed create_r_n tests

--- test/test.js
+++ test/test.js
@@ -16,11 +16,15 @@ describe('FS', function() {
     });
     
     describe('#_create_r_n(n, text)', function() {
-        it('should return a list of triplets (tuples) when n = 0', function() {
+        it('should return a list of triplets (tuples) when n = {0,1,2}', function() {
             var text = [1, 2, 2, 3, 2, 1];
             var numberText = FS._convertTextToCharPair(text);
             var r0 = FS._create_r_n(0, numberText);
             assert.equal(Math.ceil(numberText.length / 3), r0.length);
+            var r1 = FS._create_r_n(1, numberText);
+            assert.equal(Math.ceil(numberText.slice(1).length / 3), r1.length);
+            var r2 = FS._create_r_n(2, numberText);
+            assert.equal(Math.ceil(numberText.slice(2).length / 3), r2.length);
         });
     });
 });
